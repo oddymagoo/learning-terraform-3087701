ad088565e6da4c14c67518ec4a70b8883a235f59
oddymagoo <38410255+oddymagoo@users.noreply.github.com>
2024-07-26 10:38:13 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,6 +55,15 @@ resource "aws_security_group_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
+resource "aws_security_group_rule" "blog_8080_in" {
+  type        = "ingress"
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
+
 
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
